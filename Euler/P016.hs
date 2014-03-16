0dbe55a57799945443b2ddfd3f241eae273cd9e1
Sebastian Witte <woozletoff@gmail.com>
2014-03-16 23:05:33 +0100

    Solved 16

---
+++ Euler/P016.hs
@@ -0,0 +1,25 @@
+{- |
+Module      :  Euler.P016
+Description :  Problem 16
+Copyright   :  (c) Sebastian Witte
+License     :  WTFPL
+
+Maintainer  :  woozletoff@gmail.com
+Stability   :  experimental
+Portability :  portable
+
+Power digit sum
+
+215 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+
+What is the sum of the digits of the number 2^1000?
+
+-}
+module Euler.P016
+       ( solve
+       ) where
+
+import Data.Char
+
+solve :: IO ()
+solve = print . sum . fmap digitToInt . show $ 2^1000
--- Main.hs
+++ Main.hs
@@ -31,6 +31,7 @@ import qualified Euler.P012
 import qualified Euler.P013
 import qualified Euler.P014
 import qualified Euler.P015
+import qualified Euler.P016
 
 prettySolution (i, solution) =
   do putStr $ "Project Euler solution for riddle " ++ show i ++ ": "
@@ -53,4 +54,5 @@ main = mapM_ prettySolution $
                  , Euler.P013.solve
                  , Euler.P014.solve
                  , Euler.P015.solve
+                 , Euler.P016.solve
                  ]
