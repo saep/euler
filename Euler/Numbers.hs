e5febfad2d17cb024347097132a904ccc4a63e88
Sebastian Witte <woozletoff@gmail.com>
2014-04-02 15:40:42 +0200

    extracted problem 21 local definitoins to a new module Euler.Numbers

---
+++ Euler/Numbers.hs
@@ -0,0 +1,75 @@
+{- |
+Module      :  Euler.Numbers
+Description :  Number theoretic functions
+Copyright   :  (c) Sebastian Witte
+License     :  WTFPL
+
+Maintainer  :  woozletoff@gmail.com
+Stability   :  experimental
+Portability :  portable
+
+This module gather number theoretic functions.
+-}
+module Euler.Numbers
+       ( WithDivisors
+       , initialWithDivisorsState
+       , isAmicable
+       , amicableNumbersTo
+       , module Control.Monad.State
+       ) where
+
+import Control.Applicative
+import Data.Maybe
+import Euler.SList
+import Data.List (insert)
+import Euler.Prime
+import Control.Monad.State
+import qualified Data.IntMap as M
+
+-- | Simply an IntMap containing a list of Ints.
+type DivisorMap = M.IntMap [Int]
+-- | Convenience type to store a DivisorMap in a state monad.
+type WithDivisors = State DivisorMap
+
+-- | Create an initial map of divisors by creating an [1] entry for
+-- all primes up to n (exclusive).
+initialWithDivisorsState :: Int -> DivisorMap
+initialWithDivisorsState n = M.fromList $ (1,[]) : zip (takeWhile (< n) primes) (repeat [1])
+
+-- | A list of amicable numbers wrapped up in a state monad.
+amicableNumbersTo :: Int -> WithDivisors [Int]
+amicableNumbersTo n = filterM isAmicable ([2..n] `sremoveAll` primes)
+
+-- | Monady predicate to test whether the given number is an amicable number.
+isAmicable :: Int -> WithDivisors Bool
+isAmicable n = do
+  dn <- sumDivisors n
+  if n == dn
+    then return False
+    else (==n) <$> sumDivisors dn
+
+-- | Calculate the sum of divisors of n by using the (possibly)
+-- precalculated state.
+sumDivisors :: Int -> WithDivisors Int
+sumDivisors n = get >>= \m -> case M.lookup n m of
+  Just xs -> return $ sum xs
+  Nothing -> modify (mkDivisors n) >> sumDivisors n
+
+-- | Internal function to calculate a new divisormap with the value n
+-- by using the precalculated map m.
+mkDivisors :: Int -> DivisorMap -> DivisorMap
+mkDivisors n m
+  | n == fd      = M.insert n [1] m
+  | isNothing pd = mkDivisors n $ mkDivisors p m
+  | otherwise =
+    let pd' = fromJust pd ++ [p]
+        -- new divisors
+        nd = filter (\x -> x <= n && 0 ==  n `mod` x) $ fmap (*fd) pd'
+        -- proper divisors of n
+        pdn = pd' `sunion` insert fd nd
+    in M.insert n ((init . snub) pdn) m
+
+  where
+    fd = (head . factors) n -- first divisor
+    p = n `div` fd          -- previous
+    pd = M.lookup p m       -- previous divisors
--- Euler/P021.hs
+++ Euler/P021.hs
@@ -26,47 +26,8 @@ module Euler.P021
        ( solve
        ) where
 
-import Control.Applicative
-import Control.Monad.State
-import Data.Maybe
-import Euler.Prime
-import Euler.SList
-import Data.List hiding (union)
-import qualified Data.IntMap as M
-
-type WithDivisors = State (M.IntMap [Int])
+import Euler.Numbers
 
 solve :: IO ()
-solve = print . sum $ evalState (amicableNumbersTo 9999) (M.singleton 1 [])
-
-amicableNumbersTo :: Int -> WithDivisors [Int]
-amicableNumbersTo n = filterM isAmicable ([2..n] `sremoveAll` primes)
-
-isAmicable :: Int -> WithDivisors Bool
-isAmicable n = do
-  dn <- sumDivisors n
-  if n == dn
-    then return False
-    else (==n) <$> sumDivisors dn
-
-sumDivisors :: Int -> WithDivisors Int
-sumDivisors n = get >>= \m -> case M.lookup n m of
-  Just xs -> return $ sum xs
-  Nothing -> modify (mkDivisors n) >> sumDivisors n
-
-mkDivisors :: Int -> M.IntMap [Int] -> M.IntMap [Int]
-mkDivisors n m
-  | n == fd      = M.insert n [1] m
-  | isNothing pd = mkDivisors n $ mkDivisors p m
-  | otherwise =
-    let pd' = fromJust pd ++ [p]
-        -- new divisors
-        nd = filter (\x -> x <= n && 0 ==  n `mod` x) $ fmap (*fd) pd'
-        -- proper divisors of n
-        pdn = pd' `sunion` insert fd nd
-    in M.insert n ((init . snub) pdn) m
-
-  where
-    fd = (head . factors) n -- first divisor
-    p = n `div` fd          -- previous
-    pd = M.lookup p m       -- previous divisors
+solve = let limit = 9999
+        in print . sum $ evalState (amicableNumbersTo limit) (initialWithDivisorsState limit)
