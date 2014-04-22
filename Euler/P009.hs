a9123b17d119934639cbeb33b8d78e665da96af3
Sebastian Witte <woozletoff@gmail.com>
2014-04-22 10:29:04 +0200

    Refactored problem 9 to use the infinite stream of pythagorean triplets

--- Euler/P009.hs
+++ Euler/P009.hs
@@ -23,37 +23,9 @@ module Euler.P009
        ( solve
        ) where
 
-import Euler.Prime (coPrime)
+import Euler.Numbers (pythagoreanTriplets)
 
 solve :: IO ()
-solve = print . (\(a,b,c) -> a*b*c) $ findPythagoreanTriplet 3 2 1000
-
--- | The following formula describes how to calculate the pythagorean
--- triplets if m and n are coprime (i.e. their greatest common
--- denominator is 1).
---
--- Assume that m > n:
--- a = k (m^2 - n^2)
--- b = 2 k m n
--- c = k (m^2 + n^2)
---
--- So the following equatino holds:
--- k (m^2 - n^2) + 2 k m n + k (m^2 + n^2) = s
--- \<=\> k (2 m^2 + 2 m n) =  2 k m (m + n) = s
-findPythagoreanTriplet :: Int -> Int -> Int -> (Int, Int, Int)
-findPythagoreanTriplet m n s
-  | m <= n = findPythagoreanTriplet (m+1) 1 s
-  | coPrime m n && candidate == s =
-     (k * (m^2 - n^2), 2*k*m*n, k*(m^2 + n^2))
-  | otherwise = findPythagoreanTriplet m (n+1) s
-  where
-    sumConstant :: Int
-    sumConstant =  2 * m * (m+n)
-
-    candidate :: Int
-    candidate = case (dropWhile (< s) . fmap (sumConstant *)) [1..] of
-      [] -> s+1
-      ts -> head ts
-
-    k :: Int
-    k = candidate `div` sumConstant
+solve = print . (\(a,b,c) -> a*b*c) . head
+        . filter (\(a,b,c) -> a+b+c == 1000)
+        $ fmap (head . dropWhile (\(a,b,c) -> a+b+c < 1000)) pythagoreanTriplets
