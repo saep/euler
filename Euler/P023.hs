fe4fd96d6c186563d0d2527999ee17f4c78726f2
Sebastian Witte <woozletoff@gmail.com>
2014-05-10 20:56:38 +0200

    Made solution for Problem 23 fast

--- Euler/P023.hs
+++ Euler/P023.hs
@@ -1,6 +1,6 @@
 {- |
 Module      :  Euler.P023
-Description :  Problem 023
+Description :  Problem 23
 Copyright   :  (c) Sebastian Witte
 License     :  WTFPL
 
@@ -35,28 +35,52 @@ module Euler.P023
        ( solve
        ) where
 
-import qualified Data.Set as S
+import Control.Monad
 import Euler.SList
-import Euler.Numbers
 import Euler.Prime
+import qualified Data.Vector.Unboxed as VU
+import qualified Data.Vector.Unboxed.Mutable as VMU
 
-solve :: IO Int
-solve =
-  let limit = 28123
-      seed = [12..limit] `sremoveAll` primes
-      -- abundant numbers
-      as = evalState (filterM isAbundant seed) (initialWithDivisorsState limit)
-  in return $ abundantSumpairs limit as
+limit :: Int
+limit = 28123
 
--- | Given an upper limit and a list of abundant numbers, calculate
--- all the pairs of two abundant numbers.
-abundantSumpairs :: Int -> [Int] -> Int
-abundantSumpairs limit abundantNumbers =
-    S.foldl' (+) 0 . fst
-    $ foldl step (S.fromAscList [1..limit], abundantNumbers) abundantNumbers
+isAbundant :: Int -> Bool
+isAbundant n = sumOfProperDivisors n > n
+{-
+Since 18 and 12 are abundant numbers, we can conclude that any number greater
+than 18, which is evenly divisable by 6 is the composite of two abundant
+numbers. This leaves the numbers to check, which have a remainder of [1..5]. As
+20 is the next abundant number, any number with a modulo 6 remainder of 2 ≥ 32
+is also the sum of two abundant numbers. As 2*20 will yield a modulo 6 remainder
+of 4, every number ≥ 52 will also be composable of two abundant numbers.
+So every number that is divisable by 2 is the sum of to abundant numbers above
+46. So it probably saves some calculation time to manually add the even numbers
+below 46.
+-}
+solve :: Monad m => m Int
+solve = return . sum $ [1..23]
+                ++ [26] -- `mod` 6 == 2
+                ++ [28,34,46] -- `mod` 6 == 4
+                ++ [ x | x <- [25,27..limit], isNotSumOfAbundantNumbers x ]
 
-  where
-    step :: (S.Set Int, [Int]) -> Int -> (S.Set Int, [Int])
-    step (xs, a:as) n = let aps = S.fromAscList
-                                    . takeWhile (<= limit) $ fmap (+n) (a:as)
-                          in (xs S.\\ aps, as)
+isNotSumOfAbundantNumbers :: Int -> Bool
+isNotSumOfAbundantNumbers n =
+    null [ () | x <- takeWhile (< n) oddAbundantNumbers
+         , abundantNumbersVector VU.! (n-x)
+         ]
+
+oddAbundantNumbers :: [Int]
+oddAbundantNumbers = filter (abundantNumbersVector VU.!) [1,3..limit]
+
+abundantNumbersVector :: VU.Vector Bool
+abundantNumbersVector = VU.create $ do
+    v <- VMU.replicate (limit+1) False
+
+    forM_ ([12,18..limit] `sunion` [20,40..limit])
+        $ \i -> VMU.write v i True
+
+    forM_ ([13..limit] `sremoveAll` primes) $ \i -> do
+        a <- VMU.read v i
+        when (not a && isAbundant i) $ VMU.write v i True
+
+    return v
