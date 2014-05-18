37fde1142305adebc218fd7ecfa598e48586839b
Sebastian Witte <woozletoff@gmail.com>
2014-05-18 17:50:08 +0200

    Slightly improved performance for problem 12 by using an IntMap rather than a general map and removing the state monad wrapper

--- Euler/P012.hs
+++ Euler/P012.hs
@@ -38,27 +38,22 @@ module Euler.P012
        ( solve
        ) where
 
-import           Control.Monad.State
 import           Data.List
-import qualified Data.Map            as M
+import qualified Data.IntMap            as M
 import           Euler.Formulae      (triangularNumber)
 import           Euler.Prime
 
 solve :: Monad m => m Int
-solve = return $ evalState (p12 1 [2..]) M.empty
+solve = return $ p12 1 [2..] M.empty
 
-p12 :: Int -> [Int] -> State (M.Map Int [Int]) Int
-p12 dlast (x:xs) =
-  do
-    m <- get
+p12 :: Int -> [Int] -> M.IntMap [Int] -> Int
+p12 dlast (x:xs) m =
     let fx = factors x
         p = head fx
         mx = maybe fx (insert p) $ M.lookup (x `div` p) m
+        m' = M.insert x mx m
         effectiveFactors = if even x then tail mx else mx
         divisorsx = (product . fmap (succ . length) . group) effectiveFactors
-
-    modify $ M.insert x mx
-
-    if dlast*divisorsx > 500
-      then return $ triangularNumber (x-1)
-      else p12 divisorsx xs
+    in if dlast*divisorsx > 500
+        then triangularNumber (x-1)
+        else p12 divisorsx xs m'
