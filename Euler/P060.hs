ba785126b3615764a1413b43d94fd1045bd5660f
Sebastian Witte <woozletoff@gmail.com>
2014-06-02 09:25:26 +0200

    Solve 60 way simpler and faster

--- Euler/P060.hs
+++ Euler/P060.hs
@@ -24,72 +24,24 @@ module Euler.P060
        ( solve
        ) where
 
-import qualified Data.Vector.Unboxed as V
-
 import Euler.Prime
 
-import Control.Applicative
-import Control.Monad.State
-
-data BranchAndBound  = BB
-                     { currentMin         :: Int
-                     , currentMinSolution :: [Int]
-                     , primeBound         :: Int
-                     , primes'            :: [Int]
-                     , sieve              :: PrimeSieve
-                     }
-
-type Branch = State BranchAndBound
-
 solve :: Monad m => m Int
-solve = return . sum $ findConcattablePrimeSet 5
+solve = return . sum $ findNPrimePairs 5
 
-findConcattablePrimeSet :: Int -> [Int]
-findConcattablePrimeSet n = evalState (findPrimes n) initialBB
+findNPrimePairs :: Int -> [Int]
+findNPrimePairs n =
+    let ms = iterate (*10) 10000
+    in head $ concatMap (\m -> nPrimePair (takeWhile (<m) primes) n) ms
 
-initialBB :: BranchAndBound
-initialBB = BB
-          { currentMin         = maxBound
-          , currentMinSolution = []
-          , primeBound         = 10000
-          , primes'            = [ x | x <- ([3,5..primeBound initialBB])
-                                 , isPrimeS (sieve initialBB) x ]
-          , sieve              = atkin 10000
-          }
+isPrimePair :: Int -> Int -> Bool
+isPrimePair x y =
+    let bx = until (> y) (*10) 10
+        by = until (> x) (*10) 10
+    in isPrime' (x + y*by) && isPrime' (y + x*bx)
 
-instance Show BranchAndBound where
-    show bb = show (currentMin bb) ++ " " ++ show (currentMinSolution bb)
-
-
-findPrimes :: Int -> Branch [Int]
-findPrimes n = do
-    bb <- get
-    case primes' bb of
-        [] -> error "TODO could not find a solution for the current bound."
-        (p:_) | n*p > currentMin bb -> return (currentMinSolution bb)
-        (p:ps) -> do
-            findNPairs (n-1) [p] ps
-            modify $ \s -> s { primes' = ps }
-            findPrimes n
-
-findNPairs :: Int -> [Int] -> [Int] -> Branch ()
-findNPairs n pairs ps = do
-    bb <- get
-    case [ x | x <- takeWhile (\p -> p*n < (currentMin bb)) ps, all (isConcattablePrimePair x) pairs ] of
-        [] -> return ()
-        x:_ | n == 1 && sum (x:pairs) < currentMin bb ->
-            modify $ \s -> s { currentMin = sum (x:pairs), currentMinSolution = (x:pairs) }
-
-        xs -> let ps' x = [ p | p <- [x+2,x+4..primeBound bb], isPrimeS (sieve bb) p ]
-              in sequence_ $ map (\x -> findNPairs (n-1) (x:pairs) (ps' x)) xs
-
-isConcattablePrimePair :: Int -> Int -> Bool
-isConcattablePrimePair a b =
-    let exb = until (>a) (*10) 10
-        exa = until (>b) (*10) 10
-    in isPrime' (exa*a+b) && isPrime' (exb*b+a)
-
-isConcattablePrime :: (Show a1, Show a) => a1 -> a -> Bool
-isConcattablePrime p1 p2 = (isPrime' . read) (show p1 ++ show p2) && (isPrime' . read) (show p2 ++ show p1)
-
-
+nPrimePair :: [Int] -> Int -> [[Int]]
+nPrimePair _ 0 = [[]]
+nPrimePair [] _ = []
+nPrimePair (p:ps) n = [ p:xs | let nps = filter (isPrimePair p) ps
+                      , xs <- nPrimePair nps (n-1) ] ++ nPrimePair ps n
