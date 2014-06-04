74cfa9389b114ae84af2aa9e0630e657ad2e7bb0
Sebastian Witte <woozletoff@gmail.com>
2014-06-04 22:11:09 +0200

    Aded install script for sandboxes (and jenkins)

---
+++ install.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+if [ ! -x "$HOME/.cabal/bin/cabal" ] ; then
+    echo "Cabal with sandbox support (probably) not installed."
+    return 1
+fi
+
+if [ ! -f "euler.cabal" ] ; then
+    echo "Wrong working directory as MasterThesis.cabal cannot be found."
+    return 2
+fi
+
+CABAL="$HOME/.cabal/bin/cabal"
+
+if [ ! -f "./cabal.sandbox.config" ] ; then
+    $CABAL sandbox init
+fi
+
+$CABAL install --only-dependencies --enable-tests \
+    && $CABAL clean \
+    && $CABAL configure --enable-tests \
+    && $CABAL build \
+    && $CABAL test
